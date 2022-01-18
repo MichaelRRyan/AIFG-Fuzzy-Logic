f1d33e9888502ee4f58df215536b1d88465e628b
Michael R Ryan <c00239510@itcarlow.ie>
2022-01-18 21:34:42 +0000

    Added a basic implementation of the given example

--- AIFG-Fuzzy-Logic/src/main.cpp
+++ AIFG-Fuzzy-Logic/src/main.cpp
@@ -16,12 +16,52 @@
 #pragma comment(lib,"sfml-network.lib") 
 #endif
 
-#include "..\include\Game.h"
+#include <iostream>
+#include "Game.h"
+#include "Fuzzy.h"
 
 int main()
 {
-	Game game;
-	game.run();
+	//Game game;
+	//game.run();
 
-	return 0;
+	float troops = 8;
+	float range = 25.0f;
+
+	// Fuzzy force size subsets:
+	float sizeTiny = fuzzy::triangle(troops, -10.0f, 0.0f, 10.0f);
+	float sizeSmall = fuzzy::trapezoid(troops, 2.5f, 10.0f, 15.0f, 20.0f);
+	float sizeModerate = fuzzy::trapezoid(troops, 15.0f, 20.0f, 25.0f, 30.0f);
+	float sizeLarge = fuzzy::grade(troops, 25.0f, 30.0f);
+
+	// Fuzzy force distance subsets:
+	float distanceClose = fuzzy::triangle(range, -30.0f, 0.0f, 30.0f);
+	float distanceMedium = fuzzy::trapezoid(range, 10.0f, 30.0f, 50.0f, 70.0f);
+	float distanceFar = fuzzy::grade(range, 50.0f, 70.0f);
+
+	// Apply the fuzzy rules.
+	float threatLow = 
+		fuzzy::OR(fuzzy::OR(fuzzy::AND(distanceMedium, sizeTiny), 
+			fuzzy::AND(distanceMedium, sizeSmall)), 
+			fuzzy::AND(distanceFar, fuzzy::NOT(sizeLarge)));
+
+	float threatMedium = 
+		fuzzy::OR(fuzzy::OR(fuzzy::AND(distanceClose, sizeTiny), 
+			fuzzy::AND(distanceMedium, sizeModerate)), 
+			fuzzy::AND(distanceFar, sizeLarge));
+
+	float threatHigh = 
+		fuzzy::OR(fuzzy::AND(distanceClose, fuzzy::NOT(sizeTiny)), 
+			fuzzy::AND(distanceMedium, sizeLarge));
+
+	float deploy = 
+		round((threatLow * 10.0f + threatMedium * 30 + threatHigh * 50) / 
+			(threatLow + threatMedium + threatHigh));
+
+	std::cout << troops << " enemy troops spotted at distance of " << range << std::endl;
+	std::cout << "Deploying " << deploy << " troops." << std::endl;
+	
+	system("pause");
+
+	return EXIT_SUCCESS;
 }
